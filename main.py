4fdb05f84f4ec4b993a03e761347a15931d93535
leo <niels.l.larsen-1@ou.edu>
2026-03-01 11:45:04 -0600

    created foundational threading structure and subproccesses

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import subprocess
+import threading
+import queue
+import time
+
+data_queue = queue.Queue()
+
+def reciever():
+    proc = subprocess.Popen([`./example_program`], stdout = subprocess.PIPE, text=True, bufsize=1)
+def transmit():
+    proc = subprocess.Popen([`./example_program2`], stdin = subprocess.PIPE, text = True, bufsize=1)
+
+
 def main():
     print("Hello from payloadrepo2026!")
-
+    while True: 
+        #logic placed here
+        time.sleep(.01)#anti CPU death
 
 if __name__ == "__main__":
     main()
